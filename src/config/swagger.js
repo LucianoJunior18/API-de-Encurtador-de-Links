9e702830a7a8eeca57147b5e71cfc6854c0733ad
Luciano Junior <junioristylo@gmail.com>
2025-12-12 16:44:33 -0300

    docs: adiciona configuração inicial do Swagger

---
+++ src/config/swagger.js
@@ -0,0 +1,36 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API Encurtador de Links",
+            version: "1.0.0",
+            description: "Api para encurtar links e gerenciar URLs",
+            contact: {
+                name: "Squad 3",
+                email: "squad3@email.com"
+            }
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+                description: "Servidor de desenvolvimento"
+            }
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT"
+                }
+            }
+        }
+    },
+    apis: ["./src/routes/*.js"]
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export default swaggerSpec;
