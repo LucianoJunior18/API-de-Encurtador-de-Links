832e7dfee3d4d75f4503c71ee861ff814aa9262f
Luciano Junior <junioristylo@gmail.com>
2025-12-15 14:48:17 -0300

    feat: adiciona middleware de autenticação JWT

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,45 @@
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({
+            success: false,
+            message: "Token não fornecido",
+        });
+    }
+
+    const authParts = authHeader.split(" ");
+
+    if (authParts.length !== 2) {
+        return res.status(401).json({
+            success: false,
+            message: "Formato de token inválido",
+        });
+    }
+
+    const [type, token] = authParts;
+
+    if (type !== "Bearer") {
+        return res.status(401).json({
+        success: false,
+        message: "Token mal formatado",
+        });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.userId = decoded.id;
+
+    return next();
+    } catch (error) {
+    return res.status(401).json({
+        success: false,
+        message: "Token inválido ou expirado",
+    });
+    }
+};
+
+export default authMiddleware;
