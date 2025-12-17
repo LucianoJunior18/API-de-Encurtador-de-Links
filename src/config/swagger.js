3540eb81685480e1ea5b2286f4cf79fe6e9c2e85
Luciano Junior <junioristylo@gmail.com>
2025-12-17 11:47:41 -0300

    docs: melhorar descrição e contato do Swagger

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -6,10 +6,10 @@ const options = {
         info: {
             title: "API Encurtador de Links",
             version: "1.0.0",
-            description: "Api para encurtar links e gerenciar URLs",
+            description: "API REST desenvolvida para encurtar URLs longas, oferecendo autenticação segura, gerenciamento individual de links e redirecionamento público. O projeto utiliza Node.js, Express, Prisma ORM e PostgreSQL, seguindo boas práticas de arquitetura, segurança e documentação com Swagger (OpenAPI).",
             contact: {
-                name: "Squad 3",
-                email: "squad3@email.com"
+                name: "Squad 3 - Programadores do Amanhã",
+                url: "https://github.com/LucianoJunior18/API-de-Encurtador-de-Links"
             }
         },
         servers: [
