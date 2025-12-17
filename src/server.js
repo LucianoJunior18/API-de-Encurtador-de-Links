78e855f033997617912473ccfa2336e3d272273e
Luciano Junior <junioristylo@gmail.com>
2025-12-17 00:43:52 -0300

    chore: ajustes finais

--- src/server.js
+++ src/server.js
@@ -6,5 +6,7 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server rodando na porta ${PORT}`);
+  console.log(`🚀 Server rodando em: http://localhost:${PORT}`);
+  console.log(`📄 Swagger disponível em: http://localhost:${PORT}/api-docs`);
+
 });
